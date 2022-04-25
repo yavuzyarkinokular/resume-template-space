185a9c4e47e812a3e3a02ceffb88eca4e9d4a087
yavuzyarkinokular <yarkinokular@gmail.com>
2022-04-25 16:01:18 +0300

    step one done

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php include 'header.php';?>
+<!-- Add your site or app content here -->
+  <div class="hero-full-container background-image-container white-text-container" style="background-image: url('./assets/images/space.jpg')">
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="hero-full-wrapper">
+            <div class="text-content">
+              <h1>Hello,<br>
+                <span id="typed-strings">
+                  <span>I'm James Dean</span>
+                  <span>French Designer</span>
+                  <span>Working as a Freelance</span>
+                </span>
+                <span id="typed"></span>
+              </h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function (event) {
+     type();
+     movingBackgroundImage();
+  });
+</script>
+
+
+<script type="text/javascript" src="./main.70a66962.js"></script></body>
+
+</html>
